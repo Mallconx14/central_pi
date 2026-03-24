dc13029f770211263caf0a6735afcc43c31e4486
Mallconx14 <mallconx14@gmail.com>
2026-03-24 14:39:05 -0300

    deu certo

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require("express");
 const app = express();
 const mysql = require("./mysql");
 
-app.get("/pontuacao", async (req, res) => {
-    const dados = req.body;
-    const result = await mysql.execute("INSERT INTO pontuacao VALUES " + dados);
+app.post("/pontuacao", async (req, res) => {
+    const result = await mysql.execute(`
+        INSERT INTO pontuacao (nome_pista, nome_equipe, total_voltas, tempo_ultima_volta, velocidade_media, posicao)
+        VALUES
+        ('Interlagos', 'Ferrari', 55, '01:31:20', 215, 1),
+        ('Monza', 'Red Bull', 55, '01:32:05', 212, 2),
+        ('Silverstone', 'Mercedes', 54, '01:33:10', 208, 3),
+        ('Spa-Francorchamps', 'McLaren', 55, '01:31:50', 210, 4),
+        ('Suzuka', 'Ferrari', 53, '01:34:00', 205, 5);`);    
     return res.status(200).json(result);
 })
 
