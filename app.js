7ddd9025c2e8ebe37a1844ac59bb8fcce7d7cd6e
Mallconx14 <mallconx14@gmail.com>
2026-03-24 13:47:16 -0300

    fim do get

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const mysql = require("./mysql");
 
-
+app.get("/pontuacao", async (req, res) => {
+    const dados = req.body;
+    const result = await mysql.execute("INSERT INTO pontuacao VALUES " + dados);
+    return res.status(200).json(result);
+})
 
 app.use((req, res, next) => {
     const error = new Error("Not found...");
